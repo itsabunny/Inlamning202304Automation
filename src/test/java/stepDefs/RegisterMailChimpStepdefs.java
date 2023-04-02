34c39a64dc44201cc30a43e2ee974696f4384c6b
Ninis <ninis.blomerus@gmail.com>
2023-04-02 16:08:27 +0200

    Solved issues due to explicit wait

--- src/test/java/stepDefs/RegisterMailChimpStepdefs.java
+++ src/test/java/stepDefs/RegisterMailChimpStepdefs.java
@@ -5,6 +5,7 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,7 +13,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -20,10 +24,11 @@ import static org.junit.Assert.assertEquals;
 
 public class RegisterMailChimpStepdefs {
     private WebDriver driver;
-    private WebDriver wait;
+    private WebDriverWait wait;
 
     @Before
     public void SetUp() {
+
     }
 
     @After
@@ -42,14 +47,14 @@ public class RegisterMailChimpStepdefs {
             driver = new ChromeDriver(options);
             driver.manage().window().maximize();
             driver.get("https://login.mailchimp.com/signup/");
-            Thread.sleep(1000);
+            wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
         } else if (browser.equalsIgnoreCase("edge")) {
             //System.setProperty("webdriver.edge.driver", "C:/WebDrivers/msedgedriver.exe");
             driver = new EdgeDriver();
-
             driver.manage().window().maximize();
             driver.get("https://login.mailchimp.com/signup/");
-            Thread.sleep(1000);
+            wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         }
     }
 
@@ -63,59 +68,64 @@ public class RegisterMailChimpStepdefs {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
         String dateTimeString = currentDateTime.format(formatter);
 
-        // Use the formatted date and time as a username
-        String usernameTime = "user_" + dateTimeString;
-        //System.out.println("Username: " + username);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("email")));
 
         WebElement mail = driver.findElement(By.name("email"));
         mail.sendKeys(email);
         Thread.sleep(1000);
-        WebElement user = driver.findElement(By.name("username"));
-        user.sendKeys("");
-        user.clear();
-        user.sendKeys(username + dateTimeString);
-        Thread.sleep(1000);
+
         WebElement pwd = driver.findElement(By.name("password"));
         pwd.sendKeys(password);
         Thread.sleep(1000);
 
+        if (username.equalsIgnoreCase("kaninis")) {
+            WebElement user = driver.findElement(By.name("username"));
+            user.sendKeys("");
+            user.clear();
+            user.sendKeys("kaninis");
+            Thread.sleep(1000);
+
+        } else {
+            WebElement user = driver.findElement(By.name("username"));
+            user.sendKeys("");
+            user.clear();
+            user.sendKeys(username + dateTimeString);
+            Thread.sleep(1000);
+        }
     }
 
     @When("I click the Sign Up button")
     public void iClickTheSignUpButton() throws InterruptedException {
-
         WebElement button = driver.findElement(By.id("create-account-enabled"));
         Thread.sleep(2000);
-        //button.click();
         Actions actions = new Actions(driver);
-        actions.moveToElement(button).click().perform();
-        Thread.sleep(20000);
+        actions.moveToElement(button).perform();
+        button.click();
 
     }
 
     @Then("I have {string} to register")
     public void iHaveToRegister(String succeeded) throws InterruptedException {
         if (succeeded.equalsIgnoreCase("yes")) {
-
+            wait.until(ExpectedConditions.titleIs("Success | Mailchimp"));
             String expected = "Success | Mailchimp";
             String actual = driver.getTitle();
             assertEquals(expected, actual);
 
         } else if (succeeded.equalsIgnoreCase("no, long Username")) {
-
-            WebElement errorMsg = driver.findElement(By.cssSelector(".invalid-error"));
+            WebElement errorMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".invalid-error")));
             String actual = errorMsg.getText();
             String expected = "Enter a value less than 100 characters long";
             assertEquals(expected, actual);
-        } else if (succeeded.equalsIgnoreCase("no, Username taken")) {
 
-            WebElement errorMsg = driver.findElement(By.cssSelector(".invalid-error"));
+        } else if (succeeded.equalsIgnoreCase("no, Username taken")) {
+            WebElement errorMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".invalid-error")));
             String actual = errorMsg.getText();
             String expected = "Great minds think alike - someone already has this username.";
             assertEquals(true, actual.contains(expected));
-        } else if (succeeded.equalsIgnoreCase("no, no email")) {
 
-            WebElement errorMsg = driver.findElement(By.cssSelector(".invalid-error"));
+        } else if (succeeded.equalsIgnoreCase("no, no email")) {
+            WebElement errorMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".invalid-error")));
             String actual = errorMsg.getText();
             String expected = "An email address must contain a single @.";
             assertEquals(expected, actual);
